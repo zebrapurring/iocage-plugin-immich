251009ed9fcfd0f6169ae1ca8562bb2ba43c7ef8
zebrapurring <>
2024-12-06 00:29:20 +0100

    Upgrade Immich to v1.122.0

--- post_install.sh
+++ post_install.sh
@@ -7,7 +7,7 @@ IMMICH_INSTALL_DIR="/usr/local/share/immich"
 IMMICH_SETTINGS_DIR="/usr/local/etc/immich"
 IMMICH_MEDIA_DIR="/var/db/immich-media"
 IMMICH_REPO_URL="https://github.com/immich-app/immich"
-IMMICH_VERSION_TAG="v1.121.0"
+IMMICH_VERSION_TAG="v1.122.0"
 POSTGRES_PASSWORD="$(dd if=/dev/urandom bs=1 count=100 status=none | md5 -q)"
 
 # Configure PostgreSQL
