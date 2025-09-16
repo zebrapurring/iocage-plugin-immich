cce3f7553ca8c4c662b5e4f5618dbf14cdbc843d
zebrapurring <>
2025-09-16 22:00:13 +0200

    Fix patching of pnpm dependencies

--- overlay/root/immich_upgrade.sh
+++ overlay/root/immich_upgrade.sh
@@ -35,63 +35,14 @@ corepack enable pnpm
 # Build server backend
 cd "$IMMICH_REPO_DIR"
 # WA: Fix bcrypt error: `No native build was found for platform=freebsd arch=x64 runtime=node abi=127 uv=1 libc=glibc node=22.14.0`
-git apply << "EOF"
-diff --git a/pnpm-lock.yaml b/pnpm-lock.yaml
-index fa24e5b31..5ca603138 100644
---- a/pnpm-lock.yaml
-+++ b/pnpm-lock.yaml
-@@ -9,7 +9,7 @@ overrides:
-   canvas: 2.11.2
-   sharp: ^0.34.2
-
--packageExtensionsChecksum: sha256-DAYr0FTkvKYnvBH4muAER9UE1FVGKhqfRU4/QwA2xPQ=
-+packageExtensionsChecksum: sha256-3l4AQg4iuprBDup+q+2JaPvbPg/7XodWCE0ZteH+s54=
-
- pnpmfileChecksum: sha256-AG/qwrPNpmy9q60PZwCpecoYVptglTHgH+N6RKQHOM0=
-
-@@ -17631,7 +17631,10 @@ snapshots:
-   bcrypt@6.0.0:
-     dependencies:
-       node-addon-api: 8.5.0
-+      node-gyp: 11.3.0
-       node-gyp-build: 4.8.4
-+    transitivePeerDependencies:
-+      - supports-color
-
-   big.js@5.2.2: {}
-
-diff --git a/pnpm-workspace.yaml b/pnpm-workspace.yaml
-index 2114040be..ebd97f668 100644
---- a/pnpm-workspace.yaml
-+++ b/pnpm-workspace.yaml
-@@ -10,7 +10,6 @@ ignoredBuiltDependencies:
-   - '@nestjs/core'
-   - '@scarf/scarf'
-   - '@swc/core'
--  - bcrypt
-   - canvas
-   - core-js
-   - core-js-pure
-@@ -23,6 +22,7 @@ ignoredBuiltDependencies:
-   - ssh2
-   - utimes
- onlyBuiltDependencies:
-+  - bcrypt
-   - sharp
-   - '@tailwindcss/oxide'
- overrides:
-@@ -45,6 +45,10 @@ packageExtensions:
-     dependencies:
-       node-addon-api: '*'
-       node-gyp: '*'
-+  bcrypt:
-+    dependencies:
-+      node-addon-api: '*'
-+      node-gyp: '*'
-   '@immich/ui':
-     dependencies:
-       tailwindcss: '>=4.1'
-EOF
+yq -y -i '
+      .ignoredBuiltDependencies |= map(select(. != "bcrypt"))
+      | .onlyBuiltDependencies += ["bcrypt"]
+      | .onlyBuiltDependencies |= (unique)
+      | .packageExtensions.bcrypt.dependencies["node-addon-api"] = "*"
+      | .packageExtensions.bcrypt.dependencies["node-gyp"] = "*"
+   ' pnpm-workspace.yaml
+pnpm install --no-frozen-lockfile
 pnpm --filter immich --frozen-lockfile build
 pnpm --filter immich --frozen-lockfile --prod --no-optional deploy "$IMMICH_INSTALL_DIR/server"
 
